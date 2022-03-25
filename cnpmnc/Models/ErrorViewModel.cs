3d401f70748cb69a0db1f1975cba8429ae1a21b2
minhchi32 <tranminhchi0302@gmail.com>
2022-03-25 13:50:00 +0700

    Create solution structure

---
+++ cnpmnc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cnpmnc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
